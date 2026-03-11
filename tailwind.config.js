b3f9a3a6d927df85c15469fe67817d85839aadbd
Ben Irastorza <bmirastorza@gmail.com>
2026-03-10 23:41:06 -0400

    update font and descriptions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'hanken-grotesk': ['"Hanken Grotesk"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
